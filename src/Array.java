f4e5c161c81e80a1cbed72660f7457a134f78151
Karina Telles <karinatelles.codeit@sulamerica.com.br>
2022-08-12 17:21:18 -0300

    Exemplo dee uso do método build-in arraycopy, especificado na classe System que é nativa do Java
    Nesse exemplo eu copio o conteúdo do array copyFrom para o array copyTo onde o copyTo recebe o conteúdo do copyFrom a partir da posição 2 do copyFrom. O array copyTo recebe esse conteúdo do copyFrom a partir da posição 0(Recebe os valores do CopyFrom começando na posição 2 e escreve no array CopyTo a partir da posição 0) e copia 7 posições do copyFrom(array copyTo recebe os valores das 7 posições que foi copiado do copyFrom e escreve respectivamente esse valores começando na posição 0. Oresultado esperado é Cappuccino Corretto Cortado Doppio Espresso Frappucino Freddo

--- src/Array.java
+++ src/Array.java
@@ -48,5 +48,17 @@ public class Array {
         System.out.println("Element at index 8: " + anArrayOfInt[8]);
         System.out.println("Element at index 9: " + anArrayOfInt[9]);
         System.out.println(anArray.length);
+        /**Array Copy**/
+        /**Exemplo de uso do método arrayCopy especificado na classe padrão do java System**/
+        /** public static void arraycopy(Object src, int srcPos, Object dest, int destPos, int length);**/
+        /**The two Object arguments specify the array to copy from and the array to copy to.
+        The three int arguments specify the starting position in the source array, the starting position in the destination array, and the number of array elements to copy.**/
+        /**The following code declares an array of String elements. It uses the System.arraycopy() method to copy a subsequence of array components into a second array**/
+        String [] copyFrom = {
+                "Affogato", "Americano", "Cappuccino", "Corretto", "Cortado",
+                "Doppio", "Espresso", "Frappucino", "Freddo", "Lungo", "Macchiato",
+                "Marocchino", "Ristretto" };
+        String [] copyTo = new String[7];
+        System.arraycopy(copyFrom,2,copyTo,0,7);
     }
 }
